ca82d6f996da295b1b053d7c6f43110762f0723f
Pedro Anisio Silva <pedroanisio@arc4d3.com>
2023-04-23 12:19:23 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# jinja2-conf-filler
- Esse script processa templates Jinja2 usando variáveis JSON, podendo executar múltiplos jobs configurados ou uma execução ad hoc de um único template. O resultado do processamento é salvo em arquivos de saída ou exibido no console.
+# Processador de templates Jinja2 com variáveis JSON - jinja2-conf-filler
+
+## Descrição
+
+Este script processa templates Jinja2 usando variáveis JSON, podendo executar múltiplos jobs configurados ou uma execução ad hoc de um único template. O resultado do processamento é salvo em arquivos de saída ou exibido no console.
+
+## Requisitos
+
+- Python 3.6+
+- Jinja2
+
+## Instalação
+
+1. Clone o repositório:
+
+   ```
+   git clone https://github.com/pedroanisio/jinja2-conf-filler.git
+   ```
+
+2. Crie um ambiente virtual e ative-o:
+
+   ```
+   python3 -m venv venv
+   source venv/bin/activate
+   ```
+
+3. Instale as dependências:
+
+   ```
+   pip install jinja2
+   ```
+
+### Estrutura de Diretórios Padrão
+
+A estrutura de diretórios padrão do script consiste nos seguintes diretórios:
+
+- `templates`: Este diretório armazena os arquivos de template Jinja2 que serão processados. Cada arquivo deve ter a extensão `.j2`.
+- `vars`: Este diretório contém arquivos JSON com as variáveis que serão usadas no processamento dos templates. Os arquivos devem ter a extensão `.json`.
+- `output`: Este diretório armazena os arquivos de saída gerados após o processamento dos templates. O script criará subdiretórios dentro do diretório `output` conforme especificado nos jobs ou no parâmetro `-o`.
+- `jobs`: Este diretório armazena os arquivos os arquivos JSON com as configurações de jobs.
+
+Assegure-se de que os arquivos de template e variáveis estejam nos diretórios corretos antes de executar o script. Caso deseje utilizar uma estrutura de diretórios diferente, você precisará modificar o código do script para refletir os novos caminhos.   
+
+## Uso
+
+1. Coloque os arquivos de template Jinja2 no diretório `templates`.
+2. Coloque os arquivos JSON com as variáveis no diretório `vars`.
+3. Configure os jobs no arquivo `sample-job.json`, seguindo o exemplo abaixo:
+
+   ```json
+   {
+       "jobs": [
+           {
+               "job_name": "exemplo",
+               "output_dir": "output_exemplo",
+               "steps": [
+                   {
+                       "template": "template1.j2",
+                       "output": "output1.txt"
+                   },
+                   {
+                       "template": "template2.j2",
+                       "output": "output2.txt"
+                   }
+               ]
+           }
+       ]
+   }
+   ```
+
+6. Execute o script com os parâmetros desejados:
+
+   ```
+   python processador_templates.py -j jobs/sample-job.json
+   ```
+
+### Parâmetros
+
+- `-j, --jobs`: Caminho para o arquivo JSON com as configurações dos jobs (exemplo: jobs.json).
+- `-t, --template`: Nome do arquivo de template Jinja2 para uma execução ad hoc (exemplo: template.j2).
+- `-c, --console`: Imprimir o resultado no console em vez de salvar em um arquivo.
+- `-o, --output`: Nome do arquivo de saída para a execução ad hoc (exemplo: output.txt). Se não for especificado, o resultado será impresso no console.
+
+## Exemplos
+
+- Executar jobs configurados no arquivo `sample-job.json`:
+
+  ```
+  python processador_templates.py -j jobs/sample-jobs.json
+  ```
+
+- Processar um único template `template.j2` e salvar o resultado no arquivo `output.txt`:
+
+  ```
+  python processador_templates.py -t template.j2 -o output.txt
+  ```
+
+- Processar um único template `template.j2` e exibir o resultado no console:
+
+  ```
+  python processador_templates.py -t template.j2 -c
+  ```
---
+++ jinja_template_filler.py
@@ -0,0 +1,102 @@
+import argparse
+import json
+import os
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+
+def carregar_json_arquivo(caminho):
+    try:
+        with open(caminho, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        print(f'Erro na decodificação do JSON: {e}')
+        exit(1)
+    except FileNotFoundError:
+        print(f'Arquivo {caminho} não encontrado.')
+
+def carregar_json_diretorio(caminho):
+    data = {}
+    if os.path.isdir(caminho):
+        for arquivo in os.listdir(caminho):
+            if arquivo.endswith('.json'):
+                arquivo_json = os.path.join(caminho, arquivo)
+                data.update(carregar_json_arquivo(arquivo_json))
+    return data
+
+def carregar_json(caminho):
+    if os.path.isdir(caminho):
+        return carregar_json_diretorio(caminho)
+    elif os.path.isfile(caminho):
+        return carregar_json_arquivo(caminho)
+    else:
+        print(f'{caminho} não é um diretório nem um arquivo válido.')
+        return {}
+
+def processar_template(nome_template, variaveis, diretorio_template):
+    env = Environment(loader=FileSystemLoader(diretorio_template))
+    try:
+        template = env.get_template(nome_template)
+    except TemplateNotFound:
+        print(f'O arquivo de template {nome_template} não foi encontrado no diretório {diretorio_template}.')
+        exit(1)
+
+    output = template.render(variaveis)
+    return output
+
+def executar_jobs(caminho_jobs, diretorio_vars, diretorio_template, diretorio_output):
+    configuracao = carregar_json(caminho_jobs)
+    variaveis = carregar_json(diretorio_vars)
+    jobs = configuracao.get('jobs', [])
+
+    for job in jobs:
+        print(f'Executando job: {job["job_name"]}')
+        output_dir = f"{diretorio_output}/{job['output_dir']}"        
+        os.makedirs(output_dir, exist_ok=True)
+
+        for step in job['steps']:
+            nome_template = step["template"]
+            print(f'  Processando template: {nome_template}')         
+            resultado = processar_template(nome_template, variaveis, diretorio_template)
+            output_file = os.path.join(output_dir, step['output'])
+            
+            with open(output_file, 'w', encoding='utf-8') as outfile:
+                outfile.write(resultado)
+            print(f'  Arquivo gerado: {output_file}')
+
+def main():
+    parser = argparse.ArgumentParser(description='Processar arquivos de template Jinja2 com variáveis JSON e executar jobs.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    output_option = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('-j', '--jobs', help='Caminho para o arquivo JSON com as configurações dos jobs (exemplo: jobs.json).')
+    group.add_argument('-t', '--template', help='Nome do arquivo de template Jinja2 para uma execução ad hoc (exemplo: template.j2).')
+    output_option.add_argument('-c', '--console', action='store_true', help='Imprimir o resultado no console em vez de salvar em um arquivo.')
+    output_option.add_argument('-o', '--output', help='Nome do arquivo de saída para a execução ad hoc (exemplo: output.txt). Se não for especificado, o resultado será impresso no console.')
+    args = parser.parse_args()
+
+    diretorio_vars = 'vars'
+    diretorio_template = 'templates'
+    diretorio_output = 'output'
+
+    if args.jobs:
+        print('Processando o arquivo de jobs...')         
+        executar_jobs(args.jobs, diretorio_vars, diretorio_template, diretorio_output)
+    elif args.template:
+        print('Processando o template...') 
+        nome_template = args.template
+        variaveis = carregar_json(diretorio_vars)
+        resultado = processar_template(nome_template, variaveis, diretorio_template)
+
+        if args.console:
+            print('\nResultado:')
+            print(resultado)
+        else:    
+            os.makedirs(diretorio_output, exist_ok=True)
+            output_file = os.path.join(diretorio_output, args.output)
+            try:
+                with open(output_file, 'w', encoding='utf-8') as outfile:
+                    outfile.write(resultado)
+                print(f'Template processado com sucesso. Verifique o arquivo {output_file}.')
+            except IOError:
+                print(f'Erro ao escrever no arquivo de saída {output_file}.')
+
+if __name__ == '__main__':
+    main()
